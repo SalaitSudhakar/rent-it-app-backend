ad4e9c1d838460951ccd6f95844a94f3c60d45e5
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2026-03-23 13:00:42 +0530

    Add Token generation utility

---
+++ utils/generateTokens.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const accessTokenSecret = process.env.JWT_ACCESS_SECRET;
+const accessTokenExpiresIn = process.env.JWT_ACCESS_EXPIRES_IN;
+const refreshTokenSecret = process.env.JWT_REFRESH_SECRET;
+const refreshTokenExpiresIn = process.env.JWT_REFRESH_EXPIRES_IN;
+
+export function generateAccessToken(userId) {
+  if (!userId) throw new Error("User Id is missing for token generation");
+  return jwt.sign({ sub: userId }, accessTokenSecret, {
+    expiresIn: accessTokenExpiresIn,
+  });
+}
+
+export function generateRefreshToken(userId) {
+  if (!userId) throw new Error("User Id is missing for token generation");
+  return jwt.sign({ sub: userId }, refreshTokenSecret, {
+    expiresIn: refreshTokenExpiresIn,
+  });
+}

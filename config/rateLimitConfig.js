1fb573c0251eb8b7fcc20cc9142e5562892b60a7
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2026-03-23 22:37:08 +0530

    Add authLimiter config

--- config/rateLimitConfig.js
+++ config/rateLimitConfig.js
@@ -7,4 +7,10 @@ const limiter = rateLimit({
   message: "Too many requests, please try again later",
 });
 
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: "Too many requests, please try again later",
+});
+
 export default limiter;
